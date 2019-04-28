d2d6f2e1b9f4711ac2fc830b0a4a4dbe408c39df
Christian Tai Udovicic <cj.taiudovicic@gmail.com>
2019-04-27 20:49:15 -0700

    Run huge monsoon run

--- code/Convolve/array_batch_tconvolve.sh
+++ code/Convolve/array_batch_tconvolve.sh
@@ -2,10 +2,12 @@
 #SBATCH --job-name=TESS_Convolve
 #SBATCH --workdir=/common/contrib/classroom/ast520/tess_batman/code/Convolve/
 #SBATCH --output=/common/contrib/classroom/ast520/tess_batman/data/log/tconvolve%A-%a.log
-#SBATCH --time=08:00:00
-#SBATCH --mem=2000
+#SBATCH --time=80:00:00
+#SBATCH --mem=6000
 #SBATCH --account=ast520-spr19
-#SBATCH --array=1-7
+#SBATCH --array=0-97
+
+# Array must be 0-(7n-1), each setor is split into n chunks
 
 # 127850 files in Sectors 1-7
 # each tess file takes ~6 seconds on batmanCurves_small (2560 curves)
@@ -13,23 +15,36 @@
 # each task needs 21900 seconds or 6 hours
 # round up to 8 hours
 
+# Big:
+# New code is ~2secs per tess file (2560 curves)
+# Each tess file should take ~100x2s = 200 secs
+# 200*127850 = 25.6 Msecs
+# 196 tasks = 36 hrs
+# 98 tasks = 72 hrs
+
 TBP=/common/contrib/classroom/ast520/tess_batman
 echo Starting
-i=$1
-nchunks=$2
-S=$SLURM_ARRAY_TASK_ID
+
+nprocs=5
+convolvechunk=100
+
+batmansuffix=${1:-_small}
+nchunks=$(($SLURM_ARRAY_TASK_COUNT/7))
+mychunk=$(($SLURM_ARRAY_TASK_ID/7))
+S=$(($SLURM_ARRAY_TASK_ID%7+1))
 echo Working on sector $S
 
 #source activate /home/cjt347/.conda/envs/tconvolve
+module load anaconda
 source activate tconvolve
 
 N=$(ls -1q $TBP/data/TESS/Sector$S | wc -l)
 echo found $N files in sector $S
 
-let start="$i * $N / $nchunks"
-let end="($i+1) * $N / $nchunks"
-pcmd="python $TBP/code/Convolve/convolve.py $TBP/data/TESS/ $TBP/data/ $S $start $end $TBP/data/tmp/s$S/"
+let start="$mychunk * $N / $nchunks"
+let end="($mychunk+1) * $N / $nchunks"
+pcmd="python $TBP/code/Convolve/convolve.py $TBP/data/TESS/ $TBP/data/ $S $start $end $TBP/data/tmp/s$S/ $batmansuffix $nprocs $convolvechunk"
 echo running command: $pcmd
 srun $pcmd
-echo Finished
 
+echo Finished
--- code/Convolve/convolve.py
+++ code/Convolve/convolve.py
@@ -1,26 +1,44 @@
 import time
 import glob
 import os.path as p
+import multiprocessing as mp
 import numpy as np
 from scipy.signal import fftconvolve
 from astropy.io import ascii, fits
 from astropy.table import Table, Column
 
+def convolve(curves):
+    """For parallel"""
+    peak_times = np.zeros(len(curves))
+    peak_convs = np.zeros(len(curves))
+    for i,curve in enumerate(curves):
+        batman_curve = batmanCurves[curve]
+        conv = np.abs(fftconvolve(tess_flux, batman_curve, 'same'))
+        ind_max = np.argmax(conv)
+        peak_times[i] = tess_time[ind_max]
+        peak_convs[i] = conv[idxs]
 
-def tconvolve(tess_dir, batman_dir, sector, start, end, output_dir, verbosity=0):
+    # Return top 10
+    idxs = peak_convs.argsort()[-10:]
+    return peak_times[idxs], peak_convs[idxs]
+
+
+def tconvolve(tess_dir, batman_dir, batman_suffix, sector, start, end, output_dir, nprocs, chunks, verbosity=0):
     """
     
     Parameters
     ----------
     tess_dir(str): directory to TESS data
     batman_dir (str): directory to model data
+    batman_suffix(str): suffix to append to barmanCurves file (e.g. _small)
     sector (int): sector to pull data from
     start (int): file to start at
     end (int): file to end at
     output_dir (str): directory to write candidates.csv
     """
+    
     tconv_start = time.time()
-    print("===START TCONVOLVE===")
+    print("===START TCONVOLVE===",flush=True)
     
     # Handle relative paths
     tess_dir = p.abspath(tess_dir)
@@ -36,22 +54,25 @@ def tconvolve(tess_dir, batman_dir, sector, start, end, output_dir, verbosity=0)
     sector_files = glob.glob(p.join(sector_path,"*.fits"))
     tess_names = sector_files[start:end]
     ntess = len(tess_names)
-    print("Found {} TESS files to process".format(ntess))
+    print("Found {} TESS files to process".format(ntess),flush=True)
 
     # Read in Batman Curves 
     print("Reading Batman transit curves...")
-    batmanCurves = ascii.read(p.join(batman_dir,"batmanCurves_small.csv"), 
+    batmanCurves = ascii.read(p.join(batman_dir,"batmanCurves{}.csv".format(batman_suffix)), 
                        data_start=1, format='csv')
     times = np.array(batmanCurves['times'])
     curve_names = np.array(batmanCurves.colnames[1:])
     nbatman = len(curve_names)
-    print("Found {} Batman curves".format(nbatman))
+    print("Found {} Batman curves".format(nbatman),flush=True)
     
+    #Init dict for saving best batman curves 
+    d = {key : [] for key in ['sector','tessFile','curveID','tcorr','correlation']}
+    s = 0
     nerr = 0  # count number of failed files
     # Do convolution on all tess files
-    for tess_fpath in tess_names:
+    for tind,tess_fpath in enumerate(tess_names):
         tess_fname = p.basename(tess_fpath)
-        print("Starting TESS file: {}".format(tess_fname))
+        print("Starting TESS file: {}".format(tess_fname),flush=True)
         tess_start = time.time()
 
         try:
@@ -60,7 +81,7 @@ def tconvolve(tess_dir, batman_dir, sector, start, end, output_dir, verbosity=0)
                 tess_time = hdulist[1].data['TIME']
                 tess_flux = hdulist[1].data['PDCSAP_FLUX']
         except Exception as e: 
-            print("ERROR reading file: ", tess_fpath, " with error: ", e)
+            print("ERROR reading file: ", tess_fpath, " with error: ", e,flush=True)
             nerr += 1
             continue  # skip to next loop iter
         
@@ -72,52 +93,61 @@ def tconvolve(tess_dir, batman_dir, sector, start, end, output_dir, verbosity=0)
         tstd = np.std(tess_flux)
         tess_flux = (tess_flux - tmean)/tstd
         
-        # Do convolution on each batman curve
-        peak_times = np.zeros(nbatman)
-        peak_convs = np.zeros(nbatman)
+        # PARALLEL
+       # chunk = 100
+       # chunks = [curve_names[i::chunk] for i in range(chunk)]
+       # pool = mp.Pool(processes=nprocs)
+       # peak_times, peak_convs = zip(*pool.map(convolve, chunks))
+
+        # SEQUENTIAL Do convolution on each batman curve
+        num_keep = 10
+        curves = [""]*num_keep
+        times = np.zeros(num_keep)
+        convs = np.zeros(num_keep)
         conv_start = time.time()
-        print("Starting convolutions...")
-        for i, curvename in enumerate(curve_names):
+        print("Starting convolutions...",flush=True)
+        for curvename in curve_names:
             # run convolution
             # new way
             batman_curve = batmanCurves[curvename]
             conv = np.abs(fftconvolve(tess_flux, batman_curve, 'same'))
             ind_max = np.argmax(conv)
-            peak_times[i] = tess_time[ind_max]
-            peak_convs[i] = conv[ind_max]                             
+            conv_max = conv[ind_max]
+            # Only keep if in top 10
+            if conv_max > convs[-1]:
+                # insert in reverse sorted order
+                ind = np.searchsorted(-convs, -conv_max)
+                curves = curves[:ind] + [curvename] + curves[ind:-1]
+                times = np.insert(times, ind, tess_time[ind_max])[:-1]
+                convs = np.insert(convs, ind, conv_max)[:-1]
 
         conv_time = time.time() - conv_start
-        print("Convolved {} curves in {:.3} s".format(nbatman, conv_time))
+        print("Convolved {} curves in {:.3} s".format(nbatman, conv_time),flush=True)
         
-        # Keep 10 best curves
-        idxs = peak_convs.argsort()[-10:]
-        convs = peak_convs[idxs]
-        times = peak_times[idxs]
-        curves = curve_names[idxs]
-        nfitcurves = len(curves)
-        print("Found: {} fitting curves".format(nfitcurves))
-        
-        # Make table
-        if nfitcurves > 0:
-            outname = 'candidates_s{}_b{}_e{}_{}.csv'.format(sector,start,end,tess_fname)
-            outpath = p.join(output_dir, outname)
-            print("Writing table: {}".format(outname))
-            candidates = Table()
-            candidates.add_column(Column([sector_name]*nfitcurves), name="sector")
-            candidates.add_column(Column([tess_fname]*nfitcurves), name="tessFile")
-            candidates.add_column(Column(curves), name="curveID")
-            candidates.add_column(Column(times), name="tcorr")
-            candidates.add_column(Column(convs), name="correlation")
+        # Save this TESS curve's best batman curves to dict
+        d['sector'].extend([sector_name]*num_keep)
+        d['tessFile'].extend([tess_fname]*num_keep)
+        d['curveID'].extend(curves)
+        d['tcorr'].extend(times)
+        d['correlation'].extend(convs)
 
+        # Make table every 100 tess curves
+        if (tind % 10 == 0) or (tind == len(tess_names)-1):
+            e = start+tind
+            outname = 'candidates_sector{}_s{}_e{}.csv'.format(sector, s, e)
+            outpath = p.join(output_dir, outname)
+            # Convert to astropy table and write to csv
+            candidates = Table(d,names=['sector','tessFile','curveID','tcorr','correlation'])
             ascii.write(candidates, outpath, format='csv', overwrite=True, comment='#')
-            tess_time = time.time() - tess_start
-        else:
-            print("No curves found for {}, Skipping write...".format(tess_fname))
-        print("Finished TESS file in {:.3} s".format(tess_time))
-    
+            print("Wrote file {} at {} s".format(outname,time.time()-tess_start),flush=True)
+            # reset dicts
+            d = {key : [] for key in ['sector','tessFile','curveID','tcorr','correlation']}
+            columnDict = {}
+            s=e+1
+
     tconv_time = time.time() - tconv_start
-    print("Convolved {}/{} tess files with {} curves in {:.3} s".format(ntess-nerr, ntess, nbatman, tconv_time))
-    print("===END TCONVOLVE===")
+    print("Convolved {}/{} tess files with {} curves in {:.3} s".format(ntess-nerr, ntess, nbatman, tconv_time),flush=True)
+    print("===END TCONVOLVE===",flush=True)
     
 def main():
     import argparse
@@ -127,12 +157,15 @@ def main():
     parser.add_argument("sector", type=int)
     parser.add_argument("start", type=int)
     parser.add_argument("end", type=int)
-    parser.add_argument("output_dir", type=str)
+    parser.add_argument("output_dir", type=str) 
+    parser.add_argument("batman_suffix",type=str,default="")
+    parser.add_argument("nprocs", type=int)
+    parser.add_argument("chunks", type=int)
     parser.add_argument("-v", "--verbosity", default=False, 
                         action="store_true", help="Print console output")
     args = parser.parse_args()
-    tconvolve(args.tess_dir, args.batman_dir, args.sector, args.start, 
-          args.end, args.output_dir, args.verbosity)
+    tconvolve(args.tess_dir, args.batman_dir, args.batman_suffix, args.sector, args.start, 
+          args.end, args.output_dir, args.nprocs, args.chunks, args.verbosity)
           
 if __name__ == '__main__':
     main()
